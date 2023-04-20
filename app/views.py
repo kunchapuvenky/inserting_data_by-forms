5645f049954705cd71727995d1e0cefc3ba5863d
kunchapuvenky <kunchapuvenkatesh45@gmail.com>
2023-04-21 00:06:14 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from app.models import *
+from django.http import HttpResponse
+# Create your views here.
+
+
+def insert_student(request):
+    SFO=StudentForm()
+    d={'SFO':SFO}
+
+    if request.method=='POST':
+        SFD=StudentForm(request.POST)
+        if SFD.is_valid():
+            sid=SFD.cleaned_data['sid']
+            sname=SFD.cleaned_data['sname']
+            email=SFD.cleaned_data['email']
+            SO=Student.objects.get_or_create(sid=sid,sname=sname,email=email)[0]
+            SO.save()
+            
+            SQS=Student.objects.all()
+            d1={'SQS':SQS}
+            return render(request,'display_students.html',d1)
+
+    return render(request,'insert_student.html',d)
